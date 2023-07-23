917c18566f265a0859966c36ad1afdd159d3b36e
SahilGujrati <sahil.z.gujrati@slrtce.in>
2023-07-23 13:46:53 +0530

    This component groups together the header and the input fields.

--- src/components/Operations.jsx
+++ src/components/Operations.jsx
@@ -1,9 +1,13 @@
+import Header from './Header.jsx'
+import InputFields from './InputFields.jsx'
 import '../css/Operations.css'
 
 function Operations() {
     return (
-        <>
-        </>
+        <div className="operations">
+            <Header />
+            <InputFields />
+        </div>
     )
 }
 
