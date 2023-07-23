0d577d62d8a6fba1eec336ec21e56cc53f6aa446
SahilGujrati <sahil.z.gujrati@slrtce.in>
2023-07-23 13:46:09 +0530

    The header displays the name of the web app, and a short descritpion about it.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,12 @@
 import '../css/Header.css'
+
+function Header() {
+    return (
+        <header>
+            <h1>plotter</h1>
+            <p>a data visualisation tool</p>
+        </header>
+    )
+}
+
+export default Header
